a6784cdf66ffd1a6cfd2bf3623bac674b1f39562
Mike Lay <mike@mkly.io>
2015-07-27 09:56:23 -0500

    Fix file locations

--- dist.sh
+++ dist.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p dist && cd src && zip -r ../dist/under_construction.zip under_construction ../CHANGELOG.md ../LICENSE.txt
+mkdir -p dist && cp CHANGELOG.md src/under_construction/ && cp LICENSE.txt src/under_construction/ && cd src && zip -r ../dist/under_construction.zip under_construction
